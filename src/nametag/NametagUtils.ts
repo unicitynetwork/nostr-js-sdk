112f463ea65326eb1b9de471763089ab7cde5ec1
Pavel Grigorenko <pavel@unicity-labs.com>
2026-03-03 21:48:15 +0400

    fixes of fixes

--- src/nametag/NametagUtils.ts
+++ src/nametag/NametagUtils.ts
@@ -27,11 +27,7 @@ function toBase64(bytes: Uint8Array): string {
   if (typeof Buffer !== 'undefined') {
     return Buffer.from(bytes).toString('base64');
   }
-  let binary = '';
-  for (let i = 0; i < bytes.length; i++) {
-    binary += String.fromCharCode(bytes[i]);
-  }
-  return btoa(binary);
+  return btoa(String.fromCharCode(...Array.from(bytes)));
 }
 
 /**
@@ -41,12 +37,7 @@ function fromBase64(base64: string): Uint8Array {
   if (typeof Buffer !== 'undefined') {
     return new Uint8Array(Buffer.from(base64, 'base64'));
   }
-  const binary = atob(base64);
-  const bytes = new Uint8Array(binary.length);
-  for (let i = 0; i < binary.length; i++) {
-    bytes[i] = binary.charCodeAt(i);
-  }
-  return bytes;
+  return Uint8Array.from(atob(base64), c => c.charCodeAt(0));
 }
 
 /** Salt prefix for nametag hashing */
